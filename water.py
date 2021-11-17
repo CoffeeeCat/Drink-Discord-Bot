aeedb5d0118d17e230fdb0448259e7099aac6cae
CoffeeeCat <contact@peanuts-coffeestop.de>
2021-11-17 21:24:00 +0100

    Update water.py

--- water.py
+++ water.py
@@ -30,9 +30,9 @@ async def change_status():
     if msg:
         await channel.send("Danke c:, ich schreib in Zwei Stunden nochmal :3")
 
-@bot.command()
+@bot.command(pass_context=True)
 async def cat(ctx):
-    await channel.send(file=discord.File(randome.choice('cat_01.jpg', 'cat_02.jpg', 'cat_03.jpg')))
+    await ctx.send(file=discord.File(randome.choice('cat_01.jpg', 'cat_02.jpg', 'cat_03.jpg')))
 
 # Temp - URL Store
 #https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=381210&key=8E18F88DEE099EC89AEB6756C59B8B18&steamid=76561198088660842&format=json
